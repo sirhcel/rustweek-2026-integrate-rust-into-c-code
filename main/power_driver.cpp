2bf76404e4fc31e6907b79d059e65e68db864358
Christian Meusel <christian.meusel@posteo.de>
2026-05-14 15:52:54 +0200

    Clean up warning from power_driver_init
    Let's have a clean build output for easily spotting new warnings.

--- main/power_driver.cpp
+++ main/power_driver.cpp
@@ -175,9 +175,13 @@ bool power_driver_init()
 {
 #ifdef BOARD_POWERON
     ESP_LOGI(TAG, "Turn on board power pin");
-    gpio_config_t poweron_gpio_config = {0};
-    poweron_gpio_config.pin_bit_mask = 1ULL << BOARD_POWERON;
-    poweron_gpio_config.mode = GPIO_MODE_OUTPUT;
+    gpio_config_t poweron_gpio_config = {
+        .pin_bit_mask = 1ULL << BOARD_POWERON,
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
     ESP_ERROR_CHECK(gpio_config(&poweron_gpio_config));
     gpio_set_level(BOARD_POWERON, 1);
 #endif
