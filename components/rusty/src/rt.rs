4fe79e45fdd295d7bedbff2bfd30a4f04a162e87
Christian Meusel <christian.meusel@posteo.de>
2026-05-17 15:40:58 +0200

    Fix typo in comment in rusty rt.rs

--- components/rusty/src/rt.rs
+++ components/rusty/src/rt.rs
@@ -3,8 +3,8 @@ use libc_alloc::LibcAlloc;
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
-    // TODO: Provide more sophisticatd panic handling! For example, print an error message or even
-    // better propagate it to ESP-IDF panic/abort handling.
+    // TODO: Provide more sophisticated panic handling! For example, print an error message or
+    // even better propagate it to ESP-IDF panic/abort handling.
     loop {}
 }
 
